af0fd563948b6c53376ecfb2b3435f55f57584ef
Natan Ciupek <nano888@interia.eu>
2021-03-16 16:13:10 +0100

    Updates

---
+++ precache-manifest.e81ba79dea05c392468313ba9f5df2a7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "028be31bb04d4538c517",
+    "url": "/ToDoList-React/static/css/main.3594743e.chunk.css"
+  },
+  {
+    "revision": "028be31bb04d4538c517",
+    "url": "/ToDoList-React/static/js/main.d96c455a.chunk.js"
+  },
+  {
+    "revision": "bfaa32f1797b435fc275",
+    "url": "/ToDoList-React/static/js/runtime~main.88779d92.js"
+  },
+  {
+    "revision": "d8ea4e540511c59ad1e5",
+    "url": "/ToDoList-React/static/js/2.5442d947.chunk.js"
+  },
+  {
+    "revision": "d58131a94b2ab5b08b098185be928d40",
+    "url": "/ToDoList-React/index.html"
+  }
+];
